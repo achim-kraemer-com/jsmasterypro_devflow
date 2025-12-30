1677aafcaed617f62e5bdb6a46f890f54a41639f
Achim Krämer <72191461+achim-kraemer@users.noreply.github.com>
2025-12-30 12:33:01 +0100

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ const Page = () => {
     return (
         <div>
             <h1 className="text-3xl">Welcome to next.js</h1>
+            <h1 className="text-3xl font-space-grotesk">Welcome to next.js</h1>
         </div>
     )
 }
